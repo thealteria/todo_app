4f8542021d9ac9a60f0832b8c50597c45320a657
Aman Gupta <gaman100071@gmail.com>
2021-11-28 21:36:12 +0530

    Home: implemented todo timer, pause and resume functionality

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,17 +1,94 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:todo_app/app/common/constants.dart';
+import 'package:todo_app/app/common/storage/storage.dart';
+import 'package:todo_app/app/model/todo.dart';
 import 'package:todo_app/app/routes/app_pages.dart';
 
 class HomeController extends GetxController {
-  final RxList _todosList = RxList();
-  List<dynamic> get todosList => _todosList;
-  set todosList(List<dynamic> dataList) => _todosList.addAll(dataList);
+  List<Todo> todosList = <Todo>[];
+
+  Timer? _timer;
 
   @override
   void onReady() {
     super.onReady();
+
+    todosList = Storage.todoList;
+    _startTimer();
+
+    Storage.storage.listenKey(Constants.todosList, (value) {
+      todosList = value;
+
+      _timer?.cancel();
+      _startTimer();
+      update();
+    });
+  }
+
+  @override
+  void onClose() {
+    _timer?.cancel();
+
+    super.onClose();
   }
 
   void onAddTodo() {
+    Storage.todoList = todosList;
     Get.toNamed(Routes.ADD_TODO);
+  }
+
+  void _startTimer() {
+    if (todosList.isEmpty) return;
+
+    const oneSec = Duration(seconds: 1);
+
+    _timer = Timer.periodic(
+      oneSec,
+      (t) {
+        if (todosList.every((e) => e.duration == 0)) {
+          _timer?.cancel();
+          t.cancel();
+        } else {
+          for (int i = 0; i < todosList.length; i++) {
+            final t = todosList[i];
+            if (t.duration != null) {
+              if (t.duration! > 0) {
+                if (t.status == 1) {
+                  t.duration = t.duration! - 1;
+                }
+              } else {
+                t.status = 0;
+              }
+
+              updateList(t, i);
+            }
+          }
+
+          update();
+        }
+      },
+    );
+  }
+
+  void onTodoStatusChange(Todo t, int index) {
+    printInfo(info: 'todo: ${t.toJson()}');
+
+    if (t.status == 1) {
+      t.status = 2;
+    } else {
+      t.status = 1;
+    }
+
+    updateList(t, index);
+    update();
+  }
+
+  updateList(Todo t, int index) {
+    todosList[index] = t;
+    Storage.todoList = todosList;
+
+    printInfo(info: 'list: ${Storage.todoList}');
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/app/common/util/exports.dart';
+import 'package:todo_app/app/common/values/app_colors.dart';
 import 'package:todo_app/app/modules/home/controllers/home_controller.dart';
 import 'package:todo_app/app/modules/widgets/custom_app_bar.dart';
 import 'package:todo_app/app/modules/widgets/no_data_widget.dart';
@@ -20,30 +21,62 @@ class HomeView extends GetView<HomeController> {
           Icons.add,
         ),
       ),
-      body: Obx(
-        () {
-          return controller.todosList.isEmpty
+      body: GetBuilder<HomeController>(
+        builder: (c) {
+          return c.todosList.isEmpty
               ? const NoDataWidget()
               : ListView.separated(
                   separatorBuilder: (context, index) => SizedBox(height: 10.h),
-                  itemCount: controller.todosList.length,
+                  itemCount: c.todosList.length,
                   padding: EdgeInsets.all(16.w),
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
-                    final dynamic _data = controller.todosList[index];
+                    final _data = c.todosList[index];
 
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Title: ${_data['title'].toString()}',
+                    return ListTile(
+                      minLeadingWidth: 20.w,
+                      leading: Container(
+                        height: 10.w,
+                        width: 10.w,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _data.status == 1
+                              ? Colors.green
+                              : (_data.status == 2 ? Colors.grey : Colors.red),
                         ),
-                        SizedBox(height: 5.h),
-                        Text(
-                          'Body: ${_data['body'].toString()}',
+                      ),
+                      shape: OutlineInputBorder(
+                        borderRadius: 10.borderRadius,
+                        borderSide: const BorderSide(
+                          color: AppColors.doveGray,
                         ),
-                      ],
+                      ),
+                      title: Text(
+                        '${_data.title}',
+                        style: AppTextStyle.boldStyle,
+                      ),
+                      subtitle: Text(
+                        _data.status == 0
+                            ? Strings.statusDone
+                            : '${Strings.remainingDuration}'
+                                ': ${_data.duration?.formatDuration()}',
+                        style: AppTextStyle.regularStyle,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: IconButton(
+                        onPressed: () => controller.onTodoStatusChange(
+                          _data,
+                          index,
+                        ),
+                        icon: _data.status == 0
+                            ? const SizedBox.shrink()
+                            : Icon(
+                                _data.status == 1
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                              ),
+                      ),
                     );
                   },
                 );
